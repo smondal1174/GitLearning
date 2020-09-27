9ed152957f86beb81c28eba078bf2eebbf6ed09e
Subhadeep Mondal <1905829@kiit.ac.in>
2020-09-27 18:30:31 +0530

    Dev branch change 1

--- using_for_loops.c
+++ using_for_loops.c
@@ -1,6 +1,8 @@
 #include<stdio.h>
 void main(){
     for(int i=0;i<10;i++){
-        printf("i am learning git hub");
+        printf("i am learning git hub\n");
     }
+    printf("done with for loops");
+    printf("lets remove this");
 }
